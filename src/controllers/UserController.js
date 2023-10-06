4c2884eed1c7083efaf85a777740b039b964f0e5
Flávio <f.augustosdn.dev@gmail.com>
2023-10-05 23:04:10 -0300

    Implementado tratamento de erros

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,6 +1,13 @@
+const AppError = require("../utils/AppError");
+
 class UserController {
   create(request, response) {
     const { name, email, isAdmin } = request.body;
+    const verifyIsAdmin = isAdmin ? true : false;
+
+    if (!name) {
+      throw new AppError("Nome do usuário não foi informado.");
+    }
 
     return response.status(201).json({ name, email });
   }
