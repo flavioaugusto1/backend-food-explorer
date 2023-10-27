fe77744d3307f6b2bfed11123b8dec8fce275fd1
Flávio <f.augustosdn.dev@gmail.com>
2023-10-27 00:27:36 -0300

    Finalizado rota de criação de pratos

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -3,10 +3,26 @@ const knex = require("../database/knex/index.js");
 
 class DishesController {
   async create(request, response) {
-    const { name, description, image, price, category, ingredients } = request.body;
-    
-    
-    return response.status(201)
+    const { name, description, image, price, category, ingredients } =
+      request.body;
+
+    const [dishes_id] = await knex("dishes").insert({
+      name,
+      description,
+      price,
+      category,
+    });
+
+    const insertIngredients = ingredients.map(ingredient => {
+      return{
+        name: ingredient,
+        dishes_id
+      }
+    })
+
+    await knex("ingredients").insert(insertIngredients)
+
+    return response.status(201).json({message: "Prato criado com sucesso!"})
   }
 }
 
