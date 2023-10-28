6af8ed125550b99b557263b4df493c4047d9c15c
Flávio <f.augustosdn.dev@gmail.com>
2023-10-28 14:23:21 -0300

    Configurando controllers dos pratos

--- src/controllers/DishesController.js
+++ src/controllers/DishesController.js
@@ -25,6 +25,30 @@ class DishesController {
     return response.status(201).json({ message: "Prato criado com sucesso!" });
   }
 
+  async index(request, response) {
+    const { search } = request.query;
+
+    let searchDishes = await knex("dishes").select([
+      "dishes.id",
+      "dishes.name",
+      "dishes.description",
+      "dishes.price",
+      "dishes.category",
+      "dishes.image",
+    ]);
+
+    if (search) {
+      searchDishes = await knex("dishes")
+        .select("dishes.*")
+        .where("ingredients.name", "like", `%${search}%`)
+        .orWhere("dishes.name", "like", `%${search}%`)
+        .join("ingredients", "dishes.id", "ingredients.dishes_id")
+        .distinct();
+    }
+
+    return response.json({ searchDishes });
+  }
+
   async show(request, response) {
     const { id } = request.params;
 
@@ -37,6 +61,23 @@ class DishesController {
       ...dish,
       ingredients,
     });
+  }
+
+  async update(request, response) {
+    const { name, description, category, ingredients, price, image } =
+      request.body;
+    const { id } = request.params;
+    
+    // const dish = await knex("dishes").where("id", id);
+    // const listIngredients = await knex("ingredients").where("dishes_id", id)
+
+    // dish[0].name = name ?? dish[0].name
+
+    await knex("dishes").where("id", id).update({
+      name: name
+    })
+
+    return response.json({message: "Sucesso"})
   }
 
   async delete(request, response) {
--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -5,7 +5,9 @@ const dishesRoutes = Router();
 const dishesController = new DishesController();
 
 dishesRoutes.post("/register", dishesController.create);
+dishesRoutes.put("/update/:id", dishesController.update);
 dishesRoutes.get("/details/:id", dishesController.show);
+dishesRoutes.get("/all", dishesController.index);
 dishesRoutes.delete("/delete/:id", dishesController.delete);
 
 module.exports = dishesRoutes;
